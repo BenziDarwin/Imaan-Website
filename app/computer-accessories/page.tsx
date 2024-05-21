9c8e93d8d742dfd54608b97fa83d573696ca002f
BenziDarwin <ssalibenjamin0402@gmail.com>
2024-05-21 14:37:39 +0300

    Added other pages

---
+++ app/computer-accessories/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+
+function ComputerAccessories() {
+  return (
+    <main className="min-h-screen  md:p-24 px-5 pt-24">
+        ComputerAccessories</main>
+  )
+}
+
+export default ComputerAccessories
---
+++ app/laptops/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+
+function Laptops() {
+  return (
+    <main className="min-h-screen  md:p-24 px-5 pt-24">
+        Laptops</main>
+  )
+}
+
+export default Laptops
